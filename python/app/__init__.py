e1b83ea0c5a228042d143eebe038b231a65cd679
your name here <mmakhambetov@yandex.ru>
2022-02-09 02:38:01 +0400

    Create new book rating

--- python/app/__init__.py
+++ python/app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
+from sqlalchemy.exc import IntegrityError
 from .config import Configuration
 from .models import db, Rating
 
@@ -21,7 +22,7 @@ def get_book_ratings(book_id):
         .filter(Rating.book_id == book_id).all()
 
     if len(ratings) == 0:
-        return {"error": 'No ratings for this book yet.'}, 404
+        return {"Error": 'No ratings for this book yet.'}, 404
 
     total = sum([rating.value for rating in ratings])
     average_value = round(total / len(ratings), 2)
@@ -29,3 +30,23 @@ def get_book_ratings(book_id):
 
     return jsonify({"average": average_value,
                     "ratings": values})
+
+
+@app.route('/ratings/<int:book_id>', methods=['POST'])
+def rate_book(book_id):
+    if not request.args:
+        return {'Error': 'Bad data'}, 400
+    if 'value' not in request.args or 'email' not in request.args:
+        return {'Error': 'Misssing arguments'}, 400
+
+    try:
+        new_rating = {'book_id': int(book_id),
+                      'value': int(request.args['value']),
+                      'email': request.args['email']}
+        rating = Rating(**new_rating)
+        db.session.add(rating)
+        db.session.commit()
+        return jsonify({'id': rating.id, **new_rating})
+    except IntegrityError as error:
+        print(error)
+        return {'Error': 'Each user can only submit one rating per book.'}, 400
